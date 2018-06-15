de81c4f9c454b0efda7c9be2e1eaae15f6ccb744
Ina Chemerko <innachemerko@gmail.com>
2018-06-14 22:30:25 -0700

    Init Enemy and Player classes

--- js/app.js
+++ js/app.js
@@ -1,25 +1,94 @@
 // Enemies our player must avoid
-var Enemy = function() {
+//define Enemy class
+//array for enemies
+let allEnemies = [];
+
+class Enemy {
+    //assign variables x,y speed, sprite for enemy
+    constructor (x,y, speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        //The image/sprite for our enemies
+        this.sprite = 'images/enemy-bug.png';
+    }
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+    update(dt) {
+    this.x += this.speed*dt;
+}
+
+// Draw the enemy on the screen, required method for game
+    render(){
+ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+}
+
+class Player {
+    //assign variables x,y, sprite for player
+    constructor (x,y) {
+        this.x = x;
+        this.y = y;
+        //The image/sprite for our player
+        //this.sprite = 'images/char-horn-girl.png';
+        this.sprite = 'images/char-boy.png';
+        
+    }
+
+    update() {
+
+    }
+
+    // Draw the player on the screen, required method for game
+    render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput(){
+
+    }
+}
+
+//assign instances of Enemy
+let enemyFirst = new Enemy(0,65,70);
+let enemySecond = new Enemy(0,150,100);
+let enemyThird = new Enemy(0,230,80);
+allEnemies.push(enemyFirst);
+allEnemies.push(enemySecond);
+allEnemies.push(enemyThird);
+
+//assign instances of Enemy
+let player = new Player(200,410);
+/*var Enemy = function(x, y, movement) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-};
+    // variables for movement
+    this.x = x;
+    this.y = y;
+    this.movement = movement;
+
+
+}; */
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+/*Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+    this.x += this.movement*dt;
+}; */
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+/*Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+};*/
 
 // Now write your own player class
 // This class requires an update(), render() and
